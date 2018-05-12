885bd58ac2cb9aa0b3b73a7f1b2292199adddcbe
Barry Bradley <bbradley13@gmail.com>
2018-05-12 03:35:36 +0000

    Moved around methods to make more sense

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,15 +1,12 @@
 class Rook < Piece
 
-  def valid_rook_move?(new_x, new_y)
-    own_piece_at_destination?(new_x, new_y) &&
-    !is_obstructed?(new_x, new_y) &&
-    ((y_position == new_y) && (x_position != new_x) || (x_position == new_x) && (y_position !=new_y)) 
-  end
-  
   def valid_move?(new_x, new_y)
-    #If valid_move in piece.rb and valid_king_move true, then valid_move in
-    #king.rb is true
-    super(new_x, new_y) && valid_rook_move?(new_x, new_y)
+    super(new_x, new_y) && valid_rook_move?(new_x, new_y) 
+    && own_piece_at_destination?(new_x, new_y) && !is_obstructed?(new_x, new_y)
+  end
+
+  def valid_rook_move?(new_x, new_y)
+    ((y_position == new_y) && (x_position != new_x) || (x_position == new_x) && (y_position !=new_y)) 
   end
 
 end
