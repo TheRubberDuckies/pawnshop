8e832fcd21b8a5e423810b34ee3e5e63f4441b8f
Barry Bradley <bbradley13@gmail.com>
2018-05-12 04:35:55 +0000

    Changed code based on latest master

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,22 +1,14 @@
 class Queen < Piece
+	
+	def valid_move?(new_x, new_y)
+    #If valid_move in piece.rb and valid_king_move true, then valid_move in
+    #king.rb is true
+    super(new_x, new_y) && valid_queen_move?(new_x, new_y)
+  end
 
-		def valid_move?(new_x, new_y)
-    	super(new_x, new_y) && valid_queen_move?(new_x, new_y)
-  	end
-
-  	#Which version to go with?
-
-		#Nikko version of valid_queen_move?
-	  #def valid_queen_move(new_x, new_y)
-	  #  vertical_move = self.x_position === new_x && self.y_position != new_y
-	  #  horizontal_move = self.y_position === new_y && self.x_position != new_x
-	  #end
-
-		#Linh version of valid_queen_move?
-	  #def valid_queen_move?(new_x, new_y)
-	  #  return false if !super(new_x, new_y)
-	  #  horizontal_move?(new_x, new_y) ||  vertical_move?(new_x, new_y) ||  diagonal_move?(new_x, new_y)
-	  #end 
-
+  def valid_queen_move?(new_x, new_y)
+  	return false if is_obstructed?(new_x.to_i, new_y.to_i)
+    return true if (new_x.to_i - x_position).abs == (new_y.to_i - y_position).abs || new_x.to_i == x_position || new_y.to_i == y_position
+  end
+  	false
 end
-
