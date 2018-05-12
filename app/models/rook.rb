7b998762145d3162384286763ae41cf75540a422
Barry Bradley <bbradley13@gmail.com>
2018-05-12 03:25:28 +0000

    Changed valid_move to valid_rook_move and set valid_move to refer to more universal piece helper methods

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,10 +1,17 @@
 class Rook < Piece
-  def valid_move?(new_x, new_y)
+
+  def valid_rook_move?(new_x, new_y)
     own_piece_at_destination?(new_x, new_y) &&
     !is_obstructed?(new_x, new_y) &&
-    ((y_position == new_y) && (x_position != new_x) || (x_position == new_x) && (y_position !=new_y)) &&
-    !off_board?(new_x new_y)
+    ((y_position == new_y) && (x_position != new_x) || (x_position == new_x) && (y_position !=new_y)) 
   end
+  
+  def valid_move?(new_x, new_y)
+    #If valid_move in piece.rb and valid_king_move true, then valid_move in
+    #king.rb is true
+    super(new_x, new_y) && valid_rook_move?(new_x, new_y)
+  end
+
 end
     
 
